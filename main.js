8c458a524f165aa3c408c9a66b1712f404a310f3
Wooow25 <regnerischmorgen@gmail.com>
2021-06-13 22:16:40 +0300

    do it

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ const start = document.querySelector('.arenas');
 const fightButton = document.querySelector('.button');
 const winnTitle = createElem('div','loseTitle');
 const formFight = document.querySelector('.control ');
+const chat = document.querySelector('.chat')
 
 const player1 = {
     numb: '1',
@@ -23,11 +24,11 @@ const player1 = {
     hp : 100,
     img:'img/scorpion.gif',
     weapon:[],
-    act:{},
-    // attack: function(){
-    //     console.log(this.name +' Fight...')   
-    // },
-    attack: playerAttack, 
+    // act:{},
+    attack: function(){
+        console.log(this.name +' Fight...')   
+    },
+    // attack: playerAttack, 
     changeHP,
     renderHP,
     elHP,
@@ -39,8 +40,11 @@ const player2 = {
     hp : 100,
     img:'img/kitana.gif',
     weapon:[],
-    act:{},
-    attack:  enemyAttack,
+    // act:{},
+    // attack:  enemyAttack,
+    attack: function(){
+        console.log(this.name +' Fight...')   
+    },
     changeHP,
     renderHP,
     elHP,
@@ -55,10 +59,7 @@ const HIT ={
 }
 
 const ATTACK = ['head','body','foot'];
-const players =[player1, player2]
-
-
-
+// const players =[player1, player2]
 
 function createReloadButton(){
     const reloadWrap = createElem('div', "reloadWrap");
@@ -127,13 +128,16 @@ function getRandom(number){
 function fightResult(){
     if (player1.hp<=0 && player2.hp<=0){
         winnTitle.innerHTML= 'draw';
+        generateLogs( 'draw' , player1, player2);
         return true
     } else if (player1.hp<=0){
         winnTitle.innerHTML= player2.name +' win';
+        generateLogs( 'end' , player2, player1);
         return true
 
     } else if (player2.hp<=0) {
         winnTitle.innerHTML= player1.name +' win';
+        generateLogs( 'end' , player1, player2);
         return true
     } 
     return false
@@ -164,28 +168,94 @@ function playerAttack(){
     return attack
 }
 
+function generateLogs(type, p1, p2, damage=0 ){
+    const date = new Date()
+    const time = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}   `;
+    let el;
+    switch (type){
+        case 'start': {
+            const text= logs[type].replace('[time]', time).replace('[player1]',p1.name.toUpperCase()).replace('[player2]',p2.name.toUpperCase());
+            el = `<p>${text} <p>`;
+            break;
+        }
+        case 'draw': {
+            const text= logs[type]
+            el = `<p>${time} ${text} <p>`;
+            break;
+        }
+        case 'end': {
+            const text= logs[type].replace('[playerWins]',p1.name.toUpperCase()).replace('[playerLose]',p2.name.toUpperCase());
+            el = `<p>${time} ${text} <p>`;
+            break;
+        }
+        case 'hit': {
+            const text= logs[type][getRandom(logs[type].length -1)].replace('[playerKick]',p1.name.toUpperCase()).replace('[playerDefence]',p2.name.toUpperCase());
+            el = `<p>${time} ${text} Урон: ${damage} Жизни: ${p1.hp}<p>`;
+            break;
+        }
+        case 'defence':{
+            const text= logs[type][getRandom(logs[type].length -1)].replace('[playerKick]',p1.name.toUpperCase()).replace('[playerDefence]',p2.name.toUpperCase());
+            el = `<p>${time} ${text} Жизни: ${p1.hp}<p>`;
+            break;
+        }
+        default: el = 'WHAT???'
+
+    }
+
+    chat.insertAdjacentHTML('afterbegin',el)
+}
+
 start.append(createPlayer(player1))
 start.append(createPlayer(player2))
+generateLogs( 'start' , player1, player2)
 
 formFight.addEventListener('submit', function(event) {
     event.preventDefault();
-    for (let i=0;i<2;i++){
-        players[i].act=players[i].attack()
+
+    // for (let i=0;i<2;i++){
+    //     players[i].act=players[i].attack()
+    // }
+
+    // console.log('BEFORE:   '+player1.name +' '+ player1.hp+'      '+player2.name +' '+ player2.hp)
+    // console.log(player1.name +' attack '+ player1.act.hit +' with '+player1.act.value +' and defence '+player1.act.defence)
+    // console.log(player2.name +' attack '+ player2.act.hit +' with '+player2.act.value +' and defence '+player2.act.defence)
+
+    // for (let i=0;i<2;i++){
+    //     if (players[i].act.defence !== players[(i+1)%2].act.hit){
+    //         players[i].changeHP(players[(i+1)%2].act.value);
+    //         players[i].renderHP();
+    //     }
+    // }
+    
+    const enemy = enemyAttack();
+    const attack = playerAttack();
+
+
+    // console.log('BEFORE:   '+player1.name +' '+ player1.hp+'      '+player2.name +' '+ player2.hp)
+    //  console.log(player1.name +' attack '+ attack.hit +' with '+attack.value +' and defence '+attack.defence)
+    //  console.log(player2.name +' attack '+ enemy.hit +' with '+enemy.value +' and defence '+enemy.defence)
+
+
+    if (attack.defence !== enemy.hit){
+        player1.changeHP(enemy.value);
+        player1.renderHP();
+        generateLogs( 'hit' , player1, player2, enemy.value)
+    } else{
+        generateLogs( 'defence' , player2, player1)
     }
 
-    console.log('BEFORE:   '+player1.name +' '+ player1.hp+'      '+player2.name +' '+ player2.hp)
-    console.log(player1.name +' attack '+ player1.act.hit +' with '+player1.act.value +' and defence '+player1.act.defence)
-    console.log(player2.name +' attack '+ player2.act.hit +' with '+player2.act.value +' and defence '+player2.act.defence)
 
-    for (let i=0;i<2;i++){
-        if (players[i].act.defence !== players[(i+1)%2].act.hit){
-            players[i].changeHP(players[(i+1)%2].act.value);
-            players[i].renderHP();
-        }
+    if (enemy.defence !== attack.hit){
+        player2.changeHP(attack.value);
+        player2.renderHP();
+        generateLogs( 'hit', player2 , player1, attack.value)
+    }else{
+        generateLogs( 'defence', player1, player2)
     }
 
-    console.log('AFTER:   '+player1.name +' '+ player1.hp+'      '+player2.name +' '+ player2.hp)
-    console.log('____________________________')
+    // console.log('AFTER:   '+player1.name +' '+ player1.hp+'      '+player2.name +' '+ player2.hp)
+    // console.log('____________________________')
+
 
     const result =fightResult()
     if (result) {

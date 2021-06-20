ef3b8ab4b9543b8a132bbe5f6ef75212f7801d07
Wooow25 <regnerischmorgen@gmail.com>
2021-06-21 00:31:08 +0300

    good

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const $player = document.querySelector('.player');
 const $enemy = document.querySelector('.enemy');
 const $buttons = document.querySelectorAll('.button');
 const $back = document.querySelector('.backpage');
+var clicked=0;
 
 function createEmptyPlayerBlock() {
     const el = createElem('div', ['character', 'div11', 'disabled']);
@@ -15,24 +16,16 @@ function createEmptyPlayerBlock() {
 }
 
 
-const mouseMovef = (imgSrc, item, player) =>{
-    if (imgSrc === null) {
-        player.children[0].src=item.img
-        imgSrc = item.img;
+const mouseMovef = (imgSrc, item) =>{
+    if (imgSrc === null){
+        if (clicked===1){
+            $enemy.children[0].src=item.img;
+        }else if (clicked===0){
+            $player.children[0].src=item.img;
+        }
     }
 }
-const mouseOutf = (imgSrc, player) => {
-        imgSrc = null;
-        player.src = '#';
-}
-const clickf = () =>{
-    localStorage.setItem('player1', JSON.stringify(item));
-    localStorage.setItem('player2', JSON.stringify(q2));
-    el.classList.add('active');
-    setTimeout(() => {
-        window.location.pathname = 'arena.html'
-    }, 1000);
-}
+
 
 async function init() {
     localStorage.removeItem('player1');
@@ -48,86 +41,83 @@ async function init() {
     $buttons.forEach(button =>{
         button.addEventListener('click', event=>{
             const target=  event.target.innerHTML;
+            $back.style.display='none';
+            players.forEach(item => {
+            const el = createElem('div', ['character', `div${item.id}`]);
+            const img = createElem('img');
+            el.addEventListener('mousemove',function(){mouseMovef(imgSrc,item,$player)},false) 
+            img.src = item.avatar;
+            img.alt = item.name;
+            el.appendChild(img);
+            $parent.appendChild(el);
+
+
             switch (target.trim()){
                 case 'All Random': console.log('RANDOM');
-                    // Почему код здесь выдает ошибку?  Unexpected reserved word
-                    // const q1 = await fetch('https://reactmarathon-api.herokuapp.com/api/mk/player/choose').then(res => res.json());
-                    // const q2= await fetch('https://reactmarathon-api.herokuapp.com/api/mk/player/choose').then(res => res.json());
+                const elClone = el.cloneNode(true);
+                    el.parentNode.replaceChild(elClone, el);
+                    $player.children[0].src=q1.img;
+                    $enemy.children[0].src=q2.img;
                     localStorage.setItem('player1', JSON.stringify(q1));
                     localStorage.setItem('player2', JSON.stringify(q2));
                     $back.style.display='none';
+                    const fight = createElem('img','fight');
+                    fight.src='../assets/fight.gif';
+                    $parent.appendChild(fight);
+
                     setTimeout(() => {
                         window.location.pathname = 'arena.html'
-                    }, 1000);
+                    }, 2000);
                     break;
 
 
                 case 'Choose player': console.log('ONE');
-                    $back.style.display='none';
-                    players.forEach(item => {
-                    const el = createElem('div', ['character', `div${item.id}`]);
-                    const img = createElem('img');
-
-                    el.addEventListener('mousemove',function(){mouseMovef(imgSrc,item,$player)},false) 
-                    el.addEventListener('mouseout', function(){mouseOutf(imgSrc, $player)},false)
-
                     el.addEventListener('click', () => {
                         localStorage.setItem('player1', JSON.stringify(item));
+                        $enemy.children[0].src=q2.img;
                         localStorage.setItem('player2', JSON.stringify(q2));
                         el.classList.add('active');
+                        const fight = createElem('img','fight');
+                        fight.src='../assets/fight.gif';
+                        $parent.appendChild(fight);
+    
                         setTimeout(() => {
                             window.location.pathname = 'arena.html'
-                        }, 1000);
+                        }, 2000);
                     });
+                    break;
 
-                    img.src = item.avatar;
-                    img.alt = item.name;
-                    el.appendChild(img);
-                    $parent.appendChild(el);
-    });
-                 break;
+
                 case 'Choose both': console.log('TWO');
-                $back.style.display='none';
-                players.forEach(item => {
-                    const el = createElem('div', ['character', `div${item.id}`]);
-                    const img = createElem('img');
-            
-                    el.addEventListener('mousemove',function(){mouseMovef(imgSrc,item,$player)}, false) 
-                    el.addEventListener('mouseout', function(){mouseOutf(imgSrc, $player)}, false)
-                    var check = false;
                     el.addEventListener('click', () => {
-                        localStorage.setItem('player1', JSON.stringify(item));
-                        el.classList.add('active');
-                        // check =true;
-                        
-                        // setTimeout(() => {
-                        //     window.location.pathname = 'arena.html'
-                        // }, 1000);
+                        ++clicked;
+                        console.log(clicked)
+                        if (clicked===1){
+                            localStorage.setItem('player1', JSON.stringify(item));
+                            el.classList.add('active');
+                            choose = true;
+                        }
+                        if (clicked===2){
+                            localStorage.setItem('player2', JSON.stringify(item));
+                            el.classList.add('active');
+                            const fight = createElem('img','fight');
+                            fight.src='../assets/fight.gif';
+                            $parent.appendChild(fight);        
+                            setTimeout(() => {
+                                window.location.pathname = 'arena.html'
+                            }, 1000);
+
+                        }
+
                     });
-
-                    img.src = item.avatar;
-                    img.alt = item.name;
-                    el.appendChild(img);
-                    $parent.appendChild(el);
-                    
-                    if (check){
-                        const elems = document.querySelectorAll('.character')
-                        elems.forEach(character => {
-                            const elClone = character.cloneNode(true);
-                            character.parentNode.replaceChild(elClone, character);
-                        });
-                    }
-
-
-                });
-
-                 break;
+                    break;
                 default: console.log('smth wrong');
             }
         })
     })
     
     
-}
+    }
+    )}
 
 init();

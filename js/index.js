a4d8438a4047b4cb8ee7d941fff450062b984b5d
Wooow25 <regnerischmorgen@gmail.com>
2021-06-21 15:02:41 +0300

    komments

--- js/index.js
+++ js/index.js
@@ -37,11 +37,15 @@ async function init() {
     const q2= await fetch('https://reactmarathon-api.herokuapp.com/api/mk/player/choose').then(res => res.json());
 
     
-
+    // на каждую кнопку (Random, Choose Player, Choose both)
     $buttons.forEach(button =>{
+        //вешаем клик
         button.addEventListener('click', event=>{
             const target=  event.target.innerHTML;
+            //скрываем затемнение и сами кнопки
             $back.style.display='none';
+
+            //закружаем аватары и вещаем обработчик для загрузки гиф
             players.forEach(item => {
             const el = createElem('div', ['character', `div${item.id}`]);
             const img = createElem('img');
@@ -51,20 +55,25 @@ async function init() {
             el.appendChild(img);
             $parent.appendChild(el);
 
-
+            //если была нажата кнопка с текстом (далее по кейсам)
             switch (target.trim()){
+
                 case 'All Random': console.log('RANDOM');
+                //снимаем обработчик mousemove
                 const elClone = el.cloneNode(true);
                     el.parentNode.replaceChild(elClone, el);
+
+                    //загружаем gif по данным от сервера
                     $player.children[0].src=q1.img;
                     $enemy.children[0].src=q2.img;
+                    
                     localStorage.setItem('player1', JSON.stringify(q1));
                     localStorage.setItem('player2', JSON.stringify(q2));
-                    $back.style.display='none';
+                    //выводим надпись fight
                     const fight = createElem('img','fight');
                     fight.src='../assets/fight.gif';
                     $parent.appendChild(fight);
-
+                    //переход на другую страницу
                     setTimeout(() => {
                         window.location.pathname = 'arena.html'
                     }, 2000);
@@ -72,15 +81,18 @@ async function init() {
 
 
                 case 'Choose player': console.log('ONE');
+                //вещаем клики на аватар
                     el.addEventListener('click', () => {
                         localStorage.setItem('player1', JSON.stringify(item));
+                        //загружаем gif врага от сервера
                         $enemy.children[0].src=q2.img;
                         localStorage.setItem('player2', JSON.stringify(q2));
                         el.classList.add('active');
+                        //выводим надпись fight
                         const fight = createElem('img','fight');
                         fight.src='../assets/fight.gif';
                         $parent.appendChild(fight);
-    
+                         //переход на другую страницу
                         setTimeout(() => {
                             window.location.pathname = 'arena.html'
                         }, 2000);
@@ -90,19 +102,22 @@ async function init() {
 
                 case 'Choose both': console.log('TWO');
                     el.addEventListener('click', () => {
-                        ++clicked;
-                        console.log(clicked)
-                        if (clicked===1){
+                        ++clicked; 
+                        //считаем клики чтобы понимать кого мы выбираем 
+                        //велосипед, конечно, получился
+                        if (clicked===1){ //выбрали игрока
                             localStorage.setItem('player1', JSON.stringify(item));
                             el.classList.add('active');
                             choose = true;
                         }
-                        if (clicked===2){
+                        if (clicked===2){ // выбрали врага
                             localStorage.setItem('player2', JSON.stringify(item));
                             el.classList.add('active');
+                            //надпись fight
                             const fight = createElem('img','fight');
                             fight.src='../assets/fight.gif';
-                            $parent.appendChild(fight);        
+                            $parent.appendChild(fight);   
+                            //переход на другую страницу     
                             setTimeout(() => {
                                 window.location.pathname = 'arena.html'
                             }, 1000);
